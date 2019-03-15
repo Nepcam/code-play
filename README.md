8ff8141c58cf1ab384946313bfb06af3c6942585
Nepcam <cameron@aux.network>
2019-03-15 21:55:45 +1300

    having a tutu with data objects

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # code-play
 just having a tutu
+
+this is my kata - to master the practice of html css js 
---
+++ index.js
@@ -0,0 +1,23 @@
+
+let types = {
+    s: "this is a string",
+    b: false,
+    f: 1.5,
+    num: 1,
+    o: { one: 1 },
+    u: undefined,
+    fu: function f() { console.log('hi, I am a function') },
+    a: ['one', 'two', 'three'],
+    n: null
+}
+
+console.log('I am a string?', typeof types.s)
+console.log('I am a booleon?', typeof types.b)
+console.log('I am a float?', typeof types.f)
+console.log('I am a number or integer?', typeof types.num)
+console.log('I am a object?', typeof types.o)
+console.log('I am a undefined?', typeof types.u)
+console.log('I am a function?', typeof types.fu)
+console.log('I am a array?', typeof types.a)
+console.log('I am a null?', typeof types.n)
+
