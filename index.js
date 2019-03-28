bd0a8df4c115b75b6517e10098026c5fac4b3967
Nepcam <cameron@aux.network>
2019-03-29 08:59:29 +1300

    arrays and objects kata

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ let types = {
     n: null
 }
 
+/* This console.log below takes the {types} above and sifts through the object to see what the actual data type is */
+
 console.log('I am a string?', typeof types.s)
 console.log('I am a booleon?', typeof types.b)
 console.log('I am a float?', typeof types.f)
@@ -20,4 +22,22 @@ console.log('I am a undefined?', typeof types.u)
 console.log('I am a function?', typeof types.fu)
 console.log('I am a array?', typeof types.a)
 console.log('I am a null?', typeof types.n)
+
+// -----------------------------------------------------------------------------------
+
+let obj = {     //creates a variable, creating an object and connecting the two together
+    0: 'zero',
+    1: 'one',
+    2: 'two'
+}
+
+let arr = ['zero', 'one', 'two']
+
+obj[1] // calls 'one' in the obj
+arr[0] // calls zero' in the array
+
+/** when calling data in an object, the order doesn't matter because the key is named, but when calling data in an array the order does MATTER. When order matters the logic is to use an array */
+
+
+
 
